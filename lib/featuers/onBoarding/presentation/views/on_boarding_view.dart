21ef4427a3c411342c2edd00cf2b2750ddb4abee
Daly <monokakoman@gmail.com>
2024-09-21 14:51:54 +0300

    Add OnBoardingView and update SplashViewBody to navigate to OnBoardingView
    This commit introduces the `OnBoardingView` and its associated widget, `OnBoardingBody`, to the project. The `OnBoardingView` is now integrated into the route generation logic in `on_generate_routes.dart`. Additionally, the `SplashViewBody` has been updated to navigate to the `OnBoardingView` after a delay of 3 seconds. This change enhances the app's user experience by guiding users through an onboarding process after the splash screen.

---
+++ lib/featuers/onBoarding/presentation/views/on_boarding_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_app/featuers/onBoarding/presentation/views/widgets/on_boarding_body.dart';
+
+class OnBoardingView extends StatelessWidget {
+  const OnBoardingView({super.key});
+  static const String routeName = 'onBoardingView';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: OnBoardingBody(),
+    );
+  }
+}
--- lib/featuers/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/featuers/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,25 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:fruits_app/core/utils/app_images.dart';
+import 'package:fruits_app/featuers/onBoarding/presentation/views/on_boarding_view.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
