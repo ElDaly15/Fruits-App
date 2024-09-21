8703c3e075a6f61b0e72d636de2ce844537c1c38
Daly <monokakoman@gmail.com>
2024-09-21 14:26:32 +0300

    Add Splash Screen and Route Generation Logic
    Created a new splash screen and implemented route generation logic to handle navigation within the app. The `onGenerateRoute` function in `lib/core/helper/on_generate_routes.dart` manages route transitions, defaulting to the splash screen for unknown routes. The splash screen itself is defined in `lib/featuers/splash/presentation/views/splash_view.dart` and its body in `lib/featuers/splash/presentation/views/widgets/splash_view_body.dart`. Additionally, updated `lib/main.dart` to use the new route generation logic and set the initial route to the splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_app/core/helper/on_generate_routes.dart';
+import 'package:fruits_app/featuers/splash/presentation/views/splash_view.dart';
 
 void main() {
   runApp(const FruitsApp());
@@ -9,18 +11,10 @@ class FruitsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TestApp(),
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: SplashView.routeName,
     );
-  }
-}
-
-class TestApp extends StatelessWidget {
-  const TestApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
