f4a5bb40ef59e0ae3321e0af80626fe6789bea91
Daly <monokakoman@gmail.com>
2024-09-21 14:41:06 +0300

    Add SVG assets and update SplashViewBody layout
    - Integrated `flutter_svg` package to render SVG images.
    - Updated `SplashViewBody` widget to include SVG assets for the splash screen layout.
    - Added `flutter_svg` dependency to `pubspec.yaml` and updated `pubspec.lock` accordingly.
    - Organized the `SplashViewBody` widget with a column layout, including SVG images for the plant, logo, and bottom splash elements.

--- lib/featuers/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/featuers/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fruits_app/core/utils/app_images.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SvgPicture.asset(Assets.imagesPlant),
+        const Spacer(),
+        Center(child: SvgPicture.asset(Assets.imagesLogo)),
+        const Spacer(),
+        Expanded(
+          child: Center(
+              child: SvgPicture.asset(
+            Assets.imagesSplashBottom,
+            width: double.infinity,
+          )),
+        ),
+      ],
     );
   }
 }
